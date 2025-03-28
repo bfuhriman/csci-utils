472ce4fd18c59282b68b10bf5539b0cb729b16e0
Braden Fuhriman <bradywf18@gmail.com>
2025-03-27 21:17:55 -0400

    added calling command by index

--- ctest.py
+++ ctest.py
@@ -9,14 +9,27 @@ def find_pdf():
         if file.endswith(".pdf"): return file
     sys.exit("No PDF document was found in the current directory.")
 
-# Parse PDF text and run the commands in the examples section
-def run_tests(pdf):
+# Parse PDF text for shell commands
+def get_cmds(pdf):
     text = chr(12).join(page.get_text() for page in pymupdf.open(pdf)).split("\n")
-    commands = [line.strip() for line in text if line.startswith("./")]
-    for i, command in enumerate(commands):
-        print(command)
-        subprocess.run(command, shell=True, stdout=sys.stdout, stderr=sys.stderr, text=True)
-        if i < len(commands) - 1: print()
+    return [line.strip() for line in text if line.startswith("./")]
 
-pdf = find_pdf()
-run_tests(pdf)
+# Print the command, then execute it in the bash shell
+def execute(command):
+    print(command)
+    subprocess.run(command, shell=True, stdout=sys.stdout, stderr=sys.stderr, text=True)
+
+# Run tests
+def ctest(pdf):
+    commands = get_cmds(pdf)
+    if len(sys.argv) > 1:
+        if 0 < int(sys.argv[1]) < len(commands):
+            execute(commands[int(sys.argv[1]) - 1])
+        else:
+            print("Invalid command index.")
+    else:
+        for i, command in enumerate(commands):
+            execute(command) 
+            if i < len(commands) - 1: print()
+
+ctest(find_pdf())
